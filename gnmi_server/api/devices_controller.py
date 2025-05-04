92d313bcd2232cbf6aa494a016b0489bd22d2592
qraree <metbezu@gmail.com>
2025-05-05 01:22:30 +0300

    just gnmi route

--- gnmi_server/api/devices_controller.py
+++ gnmi_server/api/devices_controller.py
@@ -1,10 +1,21 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from pygnmi.client import gNMIclient
 from sqlmodel import Session, select
 
+from core.config import settings
 from core.database import engine
+from core.types.logging import logger
 from models.Device import Device, Connection
 
 device_router = APIRouter()
+
+
+@device_router.get("/connections/")
+def get_all_connections():
+    with Session(engine) as session:
+        connections = session.exec(select(Connection)).all()
+        print(connections)
+        return connections
 
 
 @device_router.get("/devices/")
@@ -14,11 +25,42 @@ def get_all_devices():
         return devices
 
 
-@device_router.get("/devices/connections/")
-def get_all_devices():
+@device_router.get("/devices/{device_id}/specs")
+def get_one_device_specs(device_id: int):
+    try:
+
+        with Session(engine) as session:
+            device = session.get(Device, device_id)
+
+            if not device:
+                raise HTTPException(status_code=404, detail="Device not found")
+
+            if not device.type == "network":
+                raise HTTPException(status_code=400, detail="Device type not supported")
+
+            target_port = device.container_host_port
+            host = (settings.lab_server, target_port)
+
+            with gNMIclient(
+                target=host, username="admin", password="admin", insecure=True
+            ) as gc:
+                result = gc.get(path=["system"])
+                print(result)
+                return result
+
+    except Exception as e:
+        logger.error(e)
+        return {"error": str(e)}
+
+
+@device_router.get("/devices/{device_id}")
+def get_one_device(device_id: int):
     with Session(engine) as session:
-        devices = session.exec(select(Connection)).all()
-        return devices
+        device = session.get(Device, device_id)
+        if not device:
+            raise HTTPException(status_code=404, detail="Device not found")
+
+        return device
 
 
 @device_router.post("/devices/")
--- gnmi_server/models/Device.py
+++ gnmi_server/models/Device.py
@@ -24,6 +24,7 @@ class Device(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     short_id: str | None = Field(default=None, nullable=True)
     name: str = Field()
+    type: str | None = Field(nullable=True)
     image: str | None = Field(nullable=True)
     state: str | None = Field(nullable=True)
     status: str | None = Field(nullable=True)
